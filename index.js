d9e5e24d9e2d1f81d542d2f52a5fc66a2500a738
Lee0701 <hgl1002@naver.com>
2020-07-05 22:00:06 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const sf = require('sf')
+const blocks = require('unicode-blocks')
+const Telegraf = require('telegraf')
+const commandParts = require('telegraf-command-parts')
+
+const token = process.env.BOT_TOKEN
+const linkFormat = process.env.LINK_FORMAT || 'https://hanja.dict.naver.com/search?query={query}'
+const formatLink = (c) => sf(linkFormat, {query: c})
+const formatMarkup = (c) => sf('[{c}]({link})', {c, link: formatLink(c)})
+
+const makeReply = (text) => text.split('')
+        .map((c) => ({c, block: blocks.fromCodePoint(c.charCodeAt(0)).name}))
+        .map(({c, block}) => block.toLowerCase().includes('ideographs') ? formatMarkup(c) : c)
+        .join('')
+
+const hanjaCommand = (ctx) => {
+    const args = ctx.state.command.args
+    const text = (ctx.message.reply_to_message) ? ctx.message.reply_to_message.text : args
+    const reply = makeReply(text || '')
+    if(reply.length > 0) ctx.reply(reply, {parse_mode: 'MarkdownV2'})
+}
+
+const whatHanja = (ctx) => {
+    const text = (ctx.message.reply_to_message) ? ctx.message.reply_to_message.text : ''
+    const reply = makeReply(text || '')
+    if(reply.length > 0) ctx.reply(reply, {parse_mode: 'MarkdownV2'})
+}
+
+const bot = new Telegraf(token)
+bot.use(commandParts())
+bot.command('hanja', hanjaCommand)
+bot.on('text', (ctx) => ctx.message.text == '무슨한자' ? whatHanja(ctx) : null)
+bot.launch()
